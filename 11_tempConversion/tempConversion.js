9b0cf87b023be5aca435c3e4180822fb17fb539a
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 12:58:28 -0500

    Complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  const C = ((F - 32) / 1.8) + 0.05
+  return Math.floor(C * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  const F = ((C * 1.8) + 32) + 0.05
+  return Math.floor(F * 10) / 10
 };
 
 // Do not edit below this line

eed8997e14faeae22e6eee3d92eb56794e7623f3
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 12:36:52 -0500

    Complete 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, number) {
+    if (number < 0) {
+        return "ERROR"
+    }
+    
+    let new_str = ""
+    for (let i = 0; i < number; i++) {
+        new_str += str
+    }
+    return new_str
 };
 
 // Do not edit below this line

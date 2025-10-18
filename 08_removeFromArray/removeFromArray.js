401fe27e7bd14e7fe5767a05d621d83eb600ebd0
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 12:43:17 -0500

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removalElements) {
+    for (elem of removalElements) {
+        let index = array.findIndex(value => value === elem)
+        while (index !== -1) {
+            array.splice(index, 1)
+            index = array.findIndex(value => value === elem)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line

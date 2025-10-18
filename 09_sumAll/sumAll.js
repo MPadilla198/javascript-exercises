8791cea28dbd64e0346e1417b89d4a8efbcfdf2b
Miguel Padilla <mpadi21@mit.edu>
2025-10-18 12:49:29 -0500

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (typeof a != "number" || typeof b != "number" ||
+        a % 1.0 != 0 || b % 1.0 != 0 ||
+         a < 0 || b < 0) {
+        return "ERROR"
+    }
+    
+    // If a is larger, then swap values between a and b
+    if (a > b) {
+        let c = a
+        a = b
+        b = c
+    }
 
+    let sum = 0
+    for (let i = a; i <= b; i++) {
+        sum += i
+    } 
+    return sum
 };
 
 // Do not edit below this line
